c4f94a1f4abc0b8427ebb1547e2c693dde1fcdfc
Jonas Raoni Soares da Silva <github@raoni.org>
2025-03-24 22:37:42 +0300

    Update DefaultManuscript.spec.js

--- cypress/tests/functional/DefaultManuscript.spec.js
+++ cypress/tests/functional/DefaultManuscript.spec.js
@@ -11,7 +11,9 @@ describe('Theme plugin tests', function() {
 	it('Enables and selects the theme', function() {
 		cy.login('admin', 'admin', 'publicknowledge');
 
-		cy.get('ul[id="navigationPrimary"] a:contains("Website")').click();
+		cy.get('nav').contains('Settings').click();
+		    // Ensure submenu item click despite animation
+		    cy.get('nav').contains('Website').click({ force: true });
 		cy.get('button[id="plugins-button"]').click();
 
 		// Find and enable the plugin
